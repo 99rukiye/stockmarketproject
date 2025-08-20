e92a41705a58fe9c2d4bc057b91ab39e706c20c2
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-20 17:14:49 +0300

    PageController

---
+++ src/main/java/com/stockmarketproject/controller/PageController.java
@@ -0,0 +1,11 @@
+package com.stockmarketproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/login")    public String login()    { return "login"; }
+    @GetMapping("/register") public String register() { return "register"; }
+    @GetMapping("/app")      public String app()      { return "app"; }
+}
