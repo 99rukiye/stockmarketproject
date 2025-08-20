9e5ba5ec47dc4dbcd971e7dab56966a8f857a397
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-20 17:15:43 +0300

    StockController

---
+++ src/main/java/com/stockmarketproject/controller/StockController.java
@@ -0,0 +1,31 @@
+package com.stockmarketproject.controller;
+
+import com.stockmarketproject.dto.StockDto;
+import com.stockmarketproject.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/stocks")
+@RequiredArgsConstructor
+public class StockController {
+
+    private final StockRepository stockRepository;
+
+    @GetMapping
+    public ResponseEntity<List<StockDto>> all() {
+        var list = stockRepository.findAll(Sort.by(Sort.Direction.ASC, "symbol"))
+                .stream().map(StockDto::from).toList();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/count")
+    public Map<String, Long> count() {
+        return Map.of("count", stockRepository.count());
+    }
+}
