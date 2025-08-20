301e7daee7accb73c7bfe1d80b6c4c9f2a1a1f26
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-20 17:01:50 +0300

    AuthController

--- src/main/java/com/stockmarketproject/controller/AuthController.java
+++ src/main/java/com/stockmarketproject/controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.stockmarketproject.controller;
-
 import com.stockmarketproject.dto.RegisterRequest;
 import com.stockmarketproject.entity.User;
 import com.stockmarketproject.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +18,12 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<User> register(@Valid @RequestBody RegisterRequest req) {
         return ResponseEntity.ok(userService.register(req.email(), req.password()));
+    }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<User> me(Authentication auth) {
+        User u = userService.getByEmail(auth.getName());
+        return ResponseEntity.ok(u);
     }
 }
