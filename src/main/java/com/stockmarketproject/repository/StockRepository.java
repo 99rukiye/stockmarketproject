2fc6e3351de981e972534eedf6db96b2545ba514
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-20 17:03:23 +0300

    StockRepository

--- src/main/java/com/stockmarketproject/repository/StockRepository.java
+++ src/main/java/com/stockmarketproject/repository/StockRepository.java
@@ -3,8 +3,14 @@ package com.stockmarketproject.repository;
 import com.stockmarketproject.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
     Optional<Stock> findBySymbol(String symbol);
+
+    List<Stock> findAllByActiveTrueOrderBySymbolAsc();
+
+    List<Stock> findAllByOrderBySymbolAsc();
 }
